e534ed89ad62829bd47729d67865e60d125b74ba
Porfireanu Constantin Laurentiu <constantin.porfireanu@student.unitbv.ro>
2024-11-03 13:19:50 +0200

    added size in order to create the parametere constructor added the default constructor in order to use it in Game class Small changes to the structrure

--- Eter/Eter/Board.cpp
+++ Eter/Eter/Board.cpp
@@ -16,8 +16,23 @@ void Board::emptyColumn(int column)
 	}
 }
 
-Board::Board(const Board& board)
+Board::Board():
+	m_size{ 0 }, m_board{} //def constructor
 {
+
+}
+
+Board::Board(uint8_t size)
+	:m_size{size}
+{
+	this->m_board.resize(size, std::vector<std::deque<int>>(size));
+}
+
+
+Board::Board(const Board& board)
+	:m_size{board.m_size},m_board{board.m_board}
+{
+
 	for (int i = 0; i < m_board.size(); i++)
 	{
 		for (int j = 0; j < m_board[i].size(); j++)
@@ -27,13 +42,15 @@ Board::Board(const Board& board)
 	}
 }
 
+
+
 Board& Board::operator=(const Board& board)
 {
 	if (this == &board)
 	{
 		return *this;
 	}
-
+	
 	for (int i = 0; i < m_board.size(); i++)
 	{
 		for (int j = 0; j < m_board[i].size(); j++)
@@ -41,6 +58,6 @@ Board& Board::operator=(const Board& board)
 			m_board[i][j] = board.m_board[i][j];
 		}
 	}
-
+	m_size = board.m_size;
 	return *this;
 }
--- Eter/Eter/Board.h
+++ Eter/Eter/Board.h
@@ -8,11 +8,14 @@ class Board
 {
 private:
 	matrix m_board;
+	uint8_t m_size;
 public:
 	void emptyRow(int row);
 	void emptyColumn(int column);
 
 	Board(const Board& board);
+	Board();//defaul constuctor
+	Board(uint8_t m_size);
 	Board& operator=(const Board& board);
 };
 
