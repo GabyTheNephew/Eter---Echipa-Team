0d47fc3020e9fe939c54a998dfd8e2d3b3c7b70e
Porfireanu Constantin Laurentiu <constantin.porfireanu@student.unitbv.ro>
2024-11-07 19:52:47 +0200

    Added a vector in player and I have added the get and set Vector methode and added the printCard so that the player knows what cards he has left.

--- Eter/Eter/Player.h
+++ Eter/Eter/Player.h
@@ -1,19 +1,26 @@
 #pragma once
 #include <string_view>
+#include<vector>
+#include "SimpleCard.h"
 class Player
 {
 private:
 	std::string_view m_name;
-	//vector for simpleCard
+	std::vector<SimpleCard> m_simpleCardsVector;
 	//vector for power
 	//vector for mage
 
 public:
 	Player();
-	Player(std::string_view name);
+	Player(std::string_view name,std::vector <SimpleCard> simpleCards);
 	~Player();
 
 	void setName(std::string_view name);
 	std::string_view getName();
+
+	void printCards();
+
+	void setVector(std::vector<SimpleCard> simpleCardsVector);
+	std::vector<SimpleCard> getVector();
 };
 
