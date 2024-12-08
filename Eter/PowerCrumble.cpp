eb8e2b2952dc2b647727c76991692231404bf840
George <georgestanga39@gmail.com>
2024-12-08 15:26:42 +0200

    added PowerCrumble

---
+++ Eter/PowerCrumble.cpp
@@ -0,0 +1,41 @@
+#include "PowerCrumble.h"
+
+PowerCrumble::PowerCrumble(std::string_view name, std::string_view description) :
+	m_name{ "Crumble" },
+	m_description{ "Value of a card is decreased by 1 (cannot be used on a ‘1’ card).  Place your marker on the card. If the chosen card is ever covered or returned to your hand, the marker is removed. In case of a tie, the marker (if present on the playing f ield), is worth -1 point." }
+{
+}
+
+std::string PowerCrumble::getName() const
+{
+	return m_name;
+}
+
+std::string PowerCrumble::getDescription() const
+{
+	return m_description;
+}
+
+void PowerCrumble::playPower(Board& board, uint8_t x, uint8_t y)
+{
+
+	board[{x, y}].back().setValue(board[{x, y}].back().getValue() + 1);
+	Color color = board[{x, y}].back().getColor();
+	if (ColorToString(color) == "Blue")
+	{
+		board[{x, y}].back().setColor(Color::decreasedBlue);
+	}
+	if (ColorToString(color) == "Red")
+	{
+		board[{x, y}].back().setColor(Color::decreasedRed);
+	}
+}
+
+bool PowerCrumble::checkPower(Board& board, uint8_t x, uint8_t y)
+{
+	if (board[{x, y}].back().getValue() == 1)
+	{
+		return false;
+	}
+	return true;
+}
