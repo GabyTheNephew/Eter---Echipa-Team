5952ac1534354f3cc0f9dc6a7d4cea588e85d45e
Porfireanu Constantin Laurentiu <constantin.porfireanu@student.unitbv.ro>
2024-12-20 18:42:02 +0200

    Added powerWave

---
+++ Eter/PowerWave.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <string_view>
+#include "Player.h"
+
+class PowerWave
+{
+private:
+	static const std::string m_name;
+	static const std::string m_description;
+public:
+	PowerWave();
+
+
+	std::string_view getName()const;
+	std::string_view getDescription()const;
+
+	bool checkWavePower(Board& board, Player& player,int16_t x,int16_t y);
+	void playWavePower(Board& board, Player& player, int16_t x, int16_t y);
+	void printEmptyAdjacentStacks(Board& board, int16_t x, int16_t y);
+};
