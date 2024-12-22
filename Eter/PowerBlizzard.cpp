63fb67eb6f1459cd3e2fa15d809d487076feb01d
George <georgestanga39@gmail.com>
2024-12-22 20:55:14 +0200

    finish PowerBlizzard

--- Eter/PowerBlizzard.cpp
+++ Eter/PowerBlizzard.cpp
@@ -19,3 +19,54 @@ std::string PowerBlizzard::getDescription() const
 	return m_description;
 }
 
+void PowerBlizzard::playBlizzardPower(Board& board, Player player1,Player player2, int16_t x, int16_t y,char position, std::vector<SimpleCard>& PastCards, std::optional<std::pair<bool, bool>>& canPlayIllusion)
+{
+	std::vector<std::pair<int16_t, int16_t>> invalidPos;
+	if (position == 'R')
+	{
+		for (int16_t i = 0; i < board.getSize(); i++)
+		{
+			invalidPos.push_back({ x,i });
+		}
+	}
+	else
+	{
+		if (position == 'C')
+		{
+			for (int16_t i = 0; i < board.getSize(); i++)
+			{
+				invalidPos.push_back({ i,y });
+			}
+		}
+	}
+
+	if (player1.numberofValidCards() > 0)
+	{
+		int16_t pozX, pozY;
+		std::cout << "Player 1's turn\n";
+		bool check = false;
+
+
+		while (check == false) 
+		{
+			SimpleCard chosenCard = player1.chooseCard();
+			std::cin >> pozX >> pozY;
+			std::pair<int, int> poz = { pozX,pozY };
+
+			if (chosenCard.getValue() != 0 && std::find(invalidPos.begin(), invalidPos.end(), poz) == invalidPos.end())
+			{
+				player2.playCard(chosenCard, board, PastCards, canPlayIllusion);
+				check = true;
+			}
+			board.print();
+
+		}
+		
+	}
+
+}
+
+bool PowerBlizzard::checkBlizzardPower(Board& board, Player& player, int16_t x, int16_t y)
+{
+	return (board.canBePlaced(x, y));
+}
